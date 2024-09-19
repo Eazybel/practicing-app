48e8755a7b49b55b43608c6463ce2bbf140cdf75
Ezedin Kamil Yassin <Eazybel27@gmail.com>
2024-09-20 01:19:18 +0300

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
 
-  function togglePopup() {
-    var popup = document.getElementById('popup');
-    if (popup.style.display === 'none') {
-      popup.style.display = 'block';
-    } else {
-      popup.style.display = 'none';
-    }
-  }
-  var slideIndex = 1;
-  showDivs(slideIndex);
-  
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-  }
-  
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-  }
-  
+ 
